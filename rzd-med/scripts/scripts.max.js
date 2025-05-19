0c0701cd580fc6e3f059fcc6c7877cd6b495852d
nikholmogorov <184939637+nikholmogorov@users.noreply.github.com>
2025-05-19 23:15:36 +0500

    Some improvements in the project "rzd-med"

--- rzd-med/scripts/scripts.max.js
+++ rzd-med/scripts/scripts.max.js
@@ -1,13 +1,13 @@
 ymaps.ready(init);
 function init() {
-    var map = new ymaps.Map('map', {
-        center: [56.292927, 43.935190],
-        zoom: 14,
-    });
-    var myPlacemark = new ymaps.Placemark(
-        [56.292927, 43.935190],
-        {
-            balloonContent: `
+	var map = new ymaps.Map('map', {
+		center: [56.292927, 43.935190],
+		zoom: 14,
+	});
+	var myPlacemark = new ymaps.Placemark(
+		[56.292927, 43.935190],
+		{
+			balloonContent: `
 				<div class="balloon">
 					<div class="balloon__address">Пр. Ленина, 18</div>
 					<div class="balloon__contacts">
@@ -15,39 +15,49 @@ function init() {
 					</div>
 				</div>
 			`,
-        },
-        {
-            iconLayout: 'default#image',
-            iconImageHref: '../images/marker.svg',
-            iconImageSize: [40, 40],
-            iconImageOffset: [-20, -40],
-        }
-    );
-    map.controls.remove('geolocationControl');
-    map.controls.remove('searchControl');
-    map.controls.remove('trafficControl');
-    map.controls.remove('typeSelector');
-    // map.controls.remove('fullscreenControl');
-    // map.controls.remove('zoomControl');
-    map.controls.remove('rulerControl');
-    map.behaviors.disable(['scrollZoom']);
-    map.geoObjects.add(myPlacemark);
-    // myPlacemark.balloon.open();
+		},
+		{
+			iconLayout: 'default#image',
+			iconImageHref: './images/marker.svg',
+			iconImageSize: [40, 40],
+			iconImageOffset: [-20, -40],
+		}
+	);
+	map.controls.remove('geolocationControl');
+	map.controls.remove('searchControl');
+	map.controls.remove('trafficControl');
+	map.controls.remove('typeSelector');
+	// map.controls.remove('fullscreenControl');
+	// map.controls.remove('zoomControl');
+	map.controls.remove('rulerControl');
+	map.behaviors.disable(['scrollZoom']);
+	map.geoObjects.add(myPlacemark);
+	// myPlacemark.balloon.open();
 };
 
 document.addEventListener("DOMContentLoaded", function () {
-    const backToTop = document.getElementById("back-to-top");
+	const backToTop = document.getElementById("back-to-top");
 
-    window.addEventListener("scroll", function () {
-        if (window.pageYOffset > 300) {
-            backToTop.style.display = "block";
-        } else {
-            backToTop.style.display = "none";
-        }
-    });
+	window.addEventListener("scroll", function () {
+		if (window.pageYOffset > 900) {
+			backToTop.style.display = "block";
+		} else {
+			backToTop.style.display = "none";
+		}
+	});
 
-    backToTop.addEventListener("click", function (event) {
-        event.preventDefault();
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    });
+	backToTop.addEventListener("click", function (event) {
+		event.preventDefault();
+		window.scrollTo({ top: 0, behavior: "smooth" });
+	});
 });
+
+window.onload = () => {
+	let userAgentString =
+		navigator.userAgent;
+	let firefoxAgent =
+		userAgentString.indexOf('Firefox') > -1;
+	if (firefoxAgent == true) {
+		document.documentElement.classList.add('scrollbar-moz-fallback');
+	}
+};
