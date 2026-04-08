2e641ed0fcc6e5f2504b99b955f999511a5183be
nikholmogorov <184939637+nikholmogorov@users.noreply.github.com>
2026-04-08 19:29:43 +0500

    trying to deploy projects

--- projects/calc/vite.config.ts
+++ projects/calc/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/projects/calc/',
+//   base: '/projects/calc/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
--- projects/todo/vite.config.ts
+++ projects/todo/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/projects/todo/',
+    // base: '/projects/todo/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
