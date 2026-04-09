6f12b0a2bcc5f0c671f348358f9c36488fbccf73
nikholmogorov <184939637+nikholmogorov@users.noreply.github.com>
2026-04-09 07:51:02 +0500

    added TS in project memory-game

--- projects/memory-game/src/components/Card.tsx
+++ projects/memory-game/src/components/Card.tsx
@@ -5,8 +5,21 @@ import cardBackDark from "../assets/images/card__back-image-dark.webp";
 import checkMark from "../assets/images/Check Mark Button.webp";
 import transparent from "../assets/images/transparent.webp";
 
-const Card = ({ src, onClick, onKeyDown, isSelected, isHidden, isAnimatedHide, theme }) => {
+interface CardProps {
+    src: string;
+    onClick: () => void;
+    onKeyDown: (e: React.KeyboardEvent<HTMLDivElement>) => void;
+    isSelected?: boolean;
+    isHidden?: boolean;
+    isAnimatedHide?: boolean;
+    theme: string;
+}
+
+const Card = (props: CardProps) => {
+    const { src, onClick, onKeyDown, isSelected, isHidden, isAnimatedHide, theme } = props;
+
     let cardBack = theme === "dark" ? cardBackDark : cardBackLight;
+
     return (
         <>
             <div className={classNames("card", { "selected": isSelected, "hidden": isHidden })} onClick={onClick} tabIndex={0} onKeyDown={onKeyDown}>
@@ -15,8 +28,7 @@ const Card = ({ src, onClick, onKeyDown, isSelected, isHidden, isAnimatedHide, t
                         loading="lazy" draggable="false" />
                 </div>
                 <div className={classNames("card__front", { "card__front--transparent": isAnimatedHide, "card__front--dark": theme === "dark" })}>
-                    <img className="card__front-image" src={isAnimatedHide ? checkMark : src} alt="Card Front" width="" height="" loading="lazy"
-                        draggable="false" />
+                    <img className="card__front-image" src={isAnimatedHide ? checkMark : src} alt="Card Front" loading="lazy" draggable="false" />
                 </div>
             </div>
         </>
