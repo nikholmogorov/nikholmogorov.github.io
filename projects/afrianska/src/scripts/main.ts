9b63fb93dea6c7ad1fb6c9cbf197ddb615d548a8
nikholmogorov <184939637+nikholmogorov@users.noreply.github.com>
2026-04-30 14:52:38 +0000

    Deploying to gh-pages from @ nikholmogorov/nikholmogorov.github.io@3469eb59bc27c84ed301a91b07f112d5caaadbeb 🚀

--- projects/afrianska/src/scripts/main.ts
+++ projects/afrianska/src/scripts/main.ts
@@ -4,9 +4,32 @@ import { FormValidator } from "./modules/FormValidator";
 import { Modal } from "./modules/Modal";
 import { Popup } from "./modules/Popup";
 import { ScrollToTopButton } from "./modules/ScrollToTopButton";
+import { EmailValidator } from "./modules/validators/EmailValidator";
+import { MessageValidator } from "./modules/validators/MessageValidator";
+import { NameValidator } from "./modules/validators/NameValidator";
+
+const letsTalkFormConfig = {
+  name: {
+    input: "[data-input='name']",
+    error: "[data-error='name']",
+    validator: new NameValidator(),
+  },
+  email: {
+    input: "[data-input='email']",
+    error: "[data-error='email']",
+    validator: new EmailValidator(),
+  },
+  message: {
+    input: "[data-input='message']",
+    error: "[data-error='message']",
+    validator: new MessageValidator(),
+  },
+};
 
 new ScrollToTopButton();
-const modal = new Modal();
+const modal = new Modal(`[data-open-modal="lets-talk"]`);
 const popup = new Popup(modal);
-const formSend = new FormSend(popup);
-new FormValidator(formSend);
+const formSend = new FormSend(
+  "form-lets-talk", "[data-submit='lets-talk']", "https://jsonplaceholder.typicode.com/posts", "POST", popup
+);
+new FormValidator("form-lets-talk", letsTalkFormConfig, formSend);
---
+++ projects/afrianska/src/scripts/modules/validators/NameValidator.ts
@@ -0,0 +1,17 @@
+/**
+* Проверка поля "name"
+ */
+
+export class NameValidator {
+
+  validate(value: string) {
+    const clearNameInputValue = value.trim().replace(/[^\p{L}\s]/gu, "");
+    if (clearNameInputValue.length === 0) {
+      return `Enter name in letters`;
+    } else if (clearNameInputValue.length < 2) {
+      return `Enter more than 1 letter`;
+    }
+    return true;
+  }
+
+}
