b99ada94503fbd25f7573d314a696e6ab69a0e70
nikholmogorov <184939637+nikholmogorov@users.noreply.github.com>
2026-09-11 11:38:37 +0500

    hotfix

--- projects/catalog/src/utils/preloadImages.ts
+++ projects/catalog/src/utils/preloadImages.ts
@@ -1,18 +1,21 @@
 import { getImageUrl } from './image';
 
-export const preloadImages = (urls, timeoutMs = 500) => {
+export const preloadImages = (urls: string[], timeoutMs: number = 500): Promise<void> => {
     if (!urls.length) return Promise.resolve();
-    const imagePromises = urls.map((url) => {
-        return new Promise((resolve) => {
+
+    const imagePromises: Promise<void>[] = urls.map((url: string) => {
+        return new Promise<void>((resolve) => {
             const img = new Image();
             img.src = getImageUrl(url);
             img.onload = () => resolve();
             img.onerror = () => resolve();
         });
     });
-    const timeoutPromise = new Promise((resolve) => {
+
+    const timeoutPromise = new Promise<void>((resolve) => {
         setTimeout(resolve, timeoutMs);
     });
+
     return Promise.race([
         Promise.all(imagePromises),
         timeoutPromise,
